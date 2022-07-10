1e26040898065cb1d5a8b92cca8b85c7aa0d0a36
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-07-10 13:42:26 -0400

    did a lot

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const { formatHtml } = require('./format-html');
+const {addWord, getStory} = require('./storyUtils');
+
+
 
 const PORT = 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', (req, res) => {
-    res.send('hello server');
+    const story = getStory();
+    res.send(formatHtml(story));
 });
 
-app.listen( PORT, () =>{
+app.post('/word', (req, res) => {
+    const newWord = req.body.word;
+    addWord(newWord);
+    const story = getStory();
+    res.send( {story} );
+})
+
+app.listen(PORT, () => {
     console.log('listening on port ' + PORT);
 })
